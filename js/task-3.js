4162ad33cc08edc9d8b037d3a82c904098987cbc
Komiedieva Kateryna <katya.komedeva00@gmail.com>
2026-01-18 01:10:59 +0100

    comments

--- js/task-3.js
+++ js/task-3.js
@@ -13,17 +13,17 @@ const profile = {
   },
 };
 
-// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-// profile.changeUsername("Marco");
-// console.log(profile.getInfo()); // "Marco has 300 active hours!"
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
-// profile.updatePlayTime(20);
-// console.log(profile.getInfo()); // "Marco has 320 active hours!"
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
 
-// Оголошена змінна profile
-// Значення змінної profile — це об’єкт з властивостями username, playTime, getInfo, changeUsername і updatePlayTime
-// Значення властивості getInfo — це функція
-// Значення властивості changeUsername — це функція
-// Значення властивості updatePlayTime — це функція
-// Для звернення до властивостей об’єкта в його методах використовується this
+Оголошена змінна profile
+Значення змінної profile — це об’єкт з властивостями username, playTime, getInfo, changeUsername і updatePlayTime
+Значення властивості getInfo — це функція
+Значення властивості changeUsername — це функція
+Значення властивості updatePlayTime — це функція
+Для звернення до властивостей об’єкта в його методах використовується this
